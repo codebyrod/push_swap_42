c5a92d039e6f545ba68c52daec6862e2e88a1d71
Rodrigo Sousa <rosousa-@student.42sp.org.br>
2025-12-19 02:41:35 -0300

    first commit

---
+++ push.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosousa- <rosousa-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/19 00:01:25 by rosousa-          #+#    #+#             */
+/*   Updated: 2025/12/19 02:14:51 by rosousa-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void push_b(t_list **head_a, t_list **head_b)
+{
+	if(!head_a)
+		return;
+
+	t_list *node_first_a;
+ 	node_first_a = *head_a;
+	*head_a = (*head_a)->next;
+	if (!head_b)
+	{
+		*head_b = node_first_a;
+		(*head_b)->next = NULL;
+	} 
+	else
+	{
+		node_first_a->next = *head_b;
+		*head_b = node_first_a;
+	}
+	ft_putendl("pb", 1); //colocar essa função ou colocar minha printf;
+}
---
+++ push_swap.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosousa- <rosousa-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/19 00:01:54 by rosousa-          #+#    #+#             */
+/*   Updated: 2025/12/19 01:20:43 by rosousa-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+
+#include <stdlib.h>
+#include <stdio.h> //APAGAR
+
+typedef struct s_list
+{
+	int id;
+	int content;
+	struct s_list *next;
+} t_list;
+
+
+
+
+#endif
