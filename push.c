38513b75a97f343bd28f8046893227544120c94b
Rodrigo Sousa <rosousa-@student.42sp.org.br>
2025-12-20 23:10:24 -0300

    building push and swap functions

--- push.c
+++ push.c
@@ -6,7 +6,7 @@
 /*   By: rosousa- <rosousa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 00:01:25 by rosousa-          #+#    #+#             */
-/*   Updated: 2025/12/19 03:32:18 by rosousa-         ###   ########.fr       */
+/*   Updated: 2025/12/20 21:42:47 by rosousa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,24 @@
 
 void push_b(t_list **head_a, t_list **head_b)
 {
+	t_list *node_first_a;
 	if(!head_a)
 		return;
-
-	t_list *node_first_a;
  	node_first_a = *head_a;
 	*head_a = (*head_a)->next;
-	if (!*head_b)
-	{
-		*head_b = node_first_a;
-		(*head_b)->next = NULL;
-	} 
-	else
-	{
-		node_first_a->next = *head_b;
-		*head_b = node_first_a;
-	} 
+	node_first_a->next = *head_b;
+	*head_b = node_first_a;
+
 	ft_putendl("pb", 1); //colocar essa função ou colocar minha printf;
+}
+
+void	push_a(t_list **head_a, t_list **head_b)
+{
+	t_list *node_first_b;
+	if(!*head_b)
+		return;
+	node_first_b = *head_b;
+	*head_b = (*head_b)->next;
+	node_first_b->next = *head_a;
+	*head_a = node_first_b;
 }
