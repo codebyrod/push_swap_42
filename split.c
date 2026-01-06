a2bd30d5045c300dd2557b01a2802b5063aa485b
Rodrigo Sousa <rosousa-@student.42sp.org.br>
2026-01-06 02:02:10 -0300

    building split

---
+++ split.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosousa- <rosousa-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/05 14:47:24 by rosousa-          #+#    #+#             */
+/*   Updated: 2026/01/05 22:01:52 by rosousa-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+char	**ft_split(char const *str, char delimiter)
+{
+	char	**new_str;
+	int		number_word;
+	int		number_letter;
+	int		i;
+	
+	i = 0;
+	if(!str)
+		return (0);
+	number_word = word_count(str, delimiter);
+	new_str = malloc ((number_word + 1) * sizeof(char *));
+	if (!new_str)
+		return (0);
+	while (number_word > i)
+	{
+		while (*str && *str == delimiter)
+			str++;
+		number_letter = letter_count(str, delimiter);
+		new_str[i] = put_content_split(str, number_letter);
+		if(!new_str[i])
+			return (ft_free_all(new_str, i));
+		str = &str[number_letter];
+		i++;
+	}
+	new_str[i] = 0;
+	return (new_str);
+}
+
+int	word_count(char const *str, char delimiter)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	if(!str)
+		return (0);
+	while (str[i])
+	{
+		if(str[i] != delimiter && (i == 0 || str[i - 1] == delimiter))
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+int	letter_count(char const *str, char delimiter)
+{
+	int	i;
+
+	i = 0;
+	if(!str)
+		return (0);
+	while (str[i] && str[i] != delimiter)
+		i++;
+	return (i);
+}
+
+char	*put_content_split(char const *str, int len)
+{
+	int i;
+	char *new_str;
+
+	i = 0;
+	if(!str)
+		return (0);
+	new_str = malloc((len + 1) * sizeof(char));
+	if (!new_str)
+		return (0);
+	while(len > i)
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	new_str[i] = 0;
+	return (new_str);
+}
+
+void	*ft_free_all(char **str, int count)
+{
+	int	i;
+
+	i = 0;
+	if(!str)
+		return (NULL);
+	while(i < count)
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+	return (NULL);
+}
